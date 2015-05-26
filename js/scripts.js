01983ed12ac1a53c9bba1a37c049922a650167c0
Alec Arme & alex caste <student@epicodus.com>
2015-05-26 16:50:46 -0700

    Added spec for multiple letters. Unable to debug complining error.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
 var cryptograph = function(inputString) {
+  var masterArray = inputString.split("");
+  var rows = 0
+  var word = ""
+  var outputArray = []
+  var outputString = ''
+  for (var columns = 1; columns * columns < masterArray.length; columns++) {
+  }
+
+  for (var x = 0; x <= masterArray.length; x++) {
+    word = word.concat(masterArray[x])
+
+    if word.length === columns {
+      outputArray.push(word);
+      var word = ""
+    };
+
+    outputArray.push(word);
+  }
+
+
+  // for (var x = 0; x < outputArray.length; x++) {
+  //     for(var y = 0; y < columns; y++) {
+  //       letter = outputArray[y].charAt(x);
+  //       if outputArray[y].charAt(x) === nil{
+  //       } else {
+  //         outputString.push(letter);
+  //       }
+  //     }
+  // }
+
 
   return inputString;
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe ('cryptograph', function() {
-  it("will return a coded message from an input", function() {
+  it("will return a coded message from an input of one letter", function() {
     expect(cryptograph("h")).to.equal("h");
+  });
+
+    it("will return a coded message from an input of three letters", function() {
+    expect(cryptograph("cat")).to.equal("cta")
   });
 });
